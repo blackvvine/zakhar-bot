733b50245935bba18fbdc71240d2885bbd9feb98
Iman Akbari <imakbari@gmail.com>
2017-07-22 02:57:59 +0430

    Unescape special characters

--- src/wikia/api.js
+++ src/wikia/api.js
@@ -1,10 +1,11 @@
 'use strict';
 
 const wikia = require('lyric-get');
+const unescape = require('entities').decodeHTML;
 
 
 function searchLyrics(artist, track, callback) {
-    wikia.get(artist, track, (err, res) => callback(err, res));
+    wikia.get(artist, track, (err, res) => callback(err, res ? unescape(res) : null));
 }
 
 
